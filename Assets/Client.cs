d6e7e1f3924111b21fc07ca08e5a46d288e17a40
Samson Choi <sh6choi@uwaterloo.ca>
2015-01-23 21:38:15 -0500

    SC - Adding functionality for client connection, client can move own cube

---
+++ Assets/Client.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Client : MonoBehaviour {
+
+	public Transform cubePrefab;
+	void OnServerInitialized(){ 
+		SpawnPlayer();
+	}
+
+	void OnConnectedToServer(){ 
+		SpawnPlayer();
+	}
+
+	void SpawnPlayer(){ 
+		Transform myTransform = (Transform)Network.Instantiate (cubePrefab, transform.position, transform.rotation, 0);
+	}
+}
