dbe3e435b72d2c55d3a16a38911f0744f693f909
Samson Choi <sh6choi@uwaterloo.ca>
2015-01-25 11:55:49 -0500

    SC/DC/EW - Adding trap functionality, as well as mana and health bars

--- Assets/Sprites/MonsterMovement.cs
+++ Assets/Sprites/MonsterMovement.cs
@@ -1,29 +1,30 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class MonsterMovement: MonoBehaviour 
+public class MonsterMovement: MonoBehaviour
 {
 	public float magnitude = 5;
 	public float speed = 5;
-	
-	private Vector3 origPos = Vector3.zero;
+	public GameObject healthBar;
 
+	private Vector3 origPos = Vector3.zero;
+	private float health = 4;
 	//DASH
 	float dashDurationCounter;
-	
+
 	bool isDashing;
-	
+
 	private int dashTapCounter;
 	private float forwardAccel;  // The current acceleration of the player
-	
+
 	private static int doubleTapTime = 60; // The amount of ticks that can occur between two key presses to double tap it
-	
+
 	private static float forwardReg = 5f;   // The ordinary acceleration
 	private static float forwardDash = 30.0f;  // The acceleration while dashing
 	private static float dashDuration = 15; //dash duration in frames
 	//CON
-	
+
 	void Start()
 	{
 		dashTapCounter = 0;
@@ -31,26 +32,26 @@ public class MonsterMovement: MonoBehaviour
 		forwardAccel = 0.0f;
 		isDashing = false;
 	}
-	
+
 	void Update()
 	{
 		if (networkView.isMine) {
 
 			//DASH
 			rigidbody2D.angularVelocity = 0;
-			
+
 			if (Input.GetButton ("Dash")) {
 				if(isDashing) {
 					rigidbody2D.velocity += new Vector2 (Input.GetAxis ("Horizontal") * forwardAccel, Input.GetAxis ("Vertical") * forwardAccel);
 				} else {
 					rigidbody2D.velocity = new Vector2(0,0);
 				}
-				
+
 			}
-			
-			
+
+
 			if (Input.GetButtonDown ("Dash") && isDashing == false) {
-				
+
 				if (dashTapCounter > 0) {
 					isDashing = true;
 					forwardAccel = forwardDash;
@@ -60,8 +61,8 @@ public class MonsterMovement: MonoBehaviour
 				}
 				dashTapCounter = doubleTapTime;
 			}
-			
-			if(isDashing){ 
+
+			if(isDashing){
 				dashDurationCounter--;
 				if(dashDurationCounter == 0){
 					isDashing = false;
@@ -70,12 +71,12 @@ public class MonsterMovement: MonoBehaviour
 					}
 				}
 			}
-			
-			
+
+
 			if (dashTapCounter > 0) {
 				dashTapCounter--;
 			}
-			
+
 			//Decelerate if going past max speed
 			if(rigidbody2D.velocity.magnitude > 10){
 				rigidbody2D.velocity = rigidbody2D.velocity * 0.7f;
@@ -85,16 +86,24 @@ public class MonsterMovement: MonoBehaviour
 			Vector3 moveDir = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
 			transform.Translate (speed * moveDir * Time.deltaTime);
 		}
+
+		healthBar.transform.localScale = new Vector3 (health / 4f, 1f, 1f);
 	}
 
 	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info) {
-		if (stream.isWriting) { 
-			Vector3 myPosition = transform.position; 
-			stream.Serialize(ref myPosition); 
-		} else { 
-			Vector3 receivedPosition = Vector3.zero; 
+		if (stream.isWriting) {
+			Vector3 myPosition = transform.position;
+			stream.Serialize(ref myPosition);
+		} else {
+			Vector3 receivedPosition = Vector3.zero;
 			stream.Serialize(ref receivedPosition);
-			transform.position = receivedPosition; 
+			transform.position = receivedPosition;
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		if (collider.name == "Trap(Clone)") {
+			health --;
 		}
 	}
 }
